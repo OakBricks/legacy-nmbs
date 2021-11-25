e6291315a55df5cd2e378073d0b8ce58674a3ddb
OakBricks <andycruzfranco@gmail.com>
2021-11-25 14:32:23 -0600

    icon plus config

---
+++ src/main/java/org/oakbricks/nmbs/mixin/WololoGoalMixin.java
@@ -0,0 +1,19 @@
+package org.oakbricks.nmbs.mixin;
+
+import net.minecraft.entity.mob.EvokerEntity;
+import org.oakbricks.nmbs.SimpleConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EvokerEntity.WololoGoal.class)
+public class WololoGoalMixin {
+	public boolean DISABLE_WOLOLO_GOAL = SimpleConfig.of("no-more-bleeding-sheep").request().getOrDefault("disableWololoGoalFromStarting", true);
+	@Inject(at = @At("HEAD"), method = "canStart()Z", cancellable = true)
+	public void canStart(CallbackInfoReturnable<Boolean> cir) {
+		if (DISABLE_WOLOLO_GOAL) {
+			cir.setReturnValue(false);
+		}
+	}
+}
